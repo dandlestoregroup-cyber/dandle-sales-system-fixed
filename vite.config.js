a2d3d28a2d73479c164e3eff5ebcdd0055801ee9
dandlestoregroup-cyber <dandlestoregroup@gmail.com>
2025-08-29 06:13:47 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // no root, no custom input — let Vite use /index.html at repo root
 })
